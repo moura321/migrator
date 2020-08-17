25275697f635ec2050e01da468c19bddbd25f1a6
Rodrigo Moura <moura321@gmail.com>
2020-08-17 13:13:52 -0300

    testes

--- run_allbenchs.sh
+++ run_allbenchs.sh
@@ -1,7 +1,8 @@
 benchmarks=('basicmath2020-03-0218:11.nvt' 'fft2020-01-191758.nvt' 'qsort2020-01-191912.nvt' 'typeset2020-01-191847.nvt')
 #'dijkstra2020-01-191729.nvt')
 
-buffer=('16' '32' '64' '128' '256')
+#buffer=('16' '32' '64' '128' '256')
+buffer=('512' '1024' '2048')
 
 counter=('4' '8' '16' '32' '64')
 
@@ -28,10 +29,10 @@ do
                        #touch "output/$bench.csv"
                     done
                 done
-            done
+            done &
             #echo $bench $buff $c
             #>> output/done.stat         
-        done &
+        done 
     done 
 done 
 
